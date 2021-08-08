69ec266b45b318d2d07e7c8f8fbfc9935a2f910e
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-08-08 12:47:08 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Sergio Sepulveda on 2021-08-08.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
