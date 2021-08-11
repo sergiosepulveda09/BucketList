10649f68b557c1efa8d3e81e0733e3523c721feb
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-08-11 15:52:51 -0700

    Communicating with a MapKit Coordinator

---
+++ BucketList/Theory/CommunicatingWithMapKit.swift
@@ -0,0 +1,20 @@
+//
+//  CommunicatingWithMapKit.swift
+//  BucketList
+//
+//  Created by Sergio Sepulveda on 2021-08-11.
+//
+
+import SwiftUI
+
+struct CommunicatingWithMapKit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommunicatingWithMapKit_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunicatingWithMapKit()
+    }
+}
--- BucketList/Theory/IntegratingMapKit/MapViewExample.swift
+++ BucketList/Theory/IntegratingMapKit/MapViewExample.swift
@@ -9,9 +9,37 @@ import SwiftUI
 import MapKit
 
 struct MapViewExample: UIViewRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapViewExample
+        
+        init(_ parent: MapViewExample) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+    }
     
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Capital of England"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+        mapView.addAnnotation(annotation)
         return mapView
     }
     func updateUIView(_ uiView: MKMapView, context: Context) {
